02a025be7053dcc6156cfbcda5a27e3636c70c09
Naillik13 <killian.galea0@orange.fr>
2020-01-27 14:27:32 +0100

    [FEAT] Add port for production

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
 
 // create express app
 const app = express();
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse requests of content-type - application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // Configuring the database
 const dbConfig = require('./config/database.config.js');
@@ -25,10 +26,10 @@ mongoose.connect(dbConfig.url, {
     process.exit();
 });
 
-const api = require("./app/routes/api")
-app.use('/api', api)
+const api = require("./app/routes/api");
+app.use('/api', api);
 
 // listen for requests
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Server is listening on port 3000");
 });
